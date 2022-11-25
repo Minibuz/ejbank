25adc4aaf1d48d4a301f38391d3bf6b4687d6be8
Max <dumerat.maxime@gmail.com>
2022-11-25 17:53:13 +0100

    Create User API and UserPayload

---
+++ EJBank-web/src/main/java/com/ejbank/api/User.java
@@ -0,0 +1,28 @@
+package com.ejbank.api;
+
+import com.ejbank.api.payload.UserPayload;
+import com.ejbank.test.TestBeanLocal;
+
+import javax.ejb.EJB;
+import javax.enterprise.context.RequestScoped;
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+
+@Path("/user")
+@Produces(MediaType.APPLICATION_JSON)
+@RequestScoped
+public class User {
+
+    @EJB
+    private TestBeanLocal testBean;
+
+    @GET
+    @Path("/{user_id}")
+    public UserPayload UserPayloadReponse(@PathParam("user_id") Long id) {
+        //get information form Bean User
+        //var result = UserBean.find(id)
+        var result = new UserPayload("Jean", "Dupont");
+        return result;
+    }
+
+}
