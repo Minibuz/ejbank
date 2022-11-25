f3e462f23eff0ca45290e873998a9b4ac83bc389
Max <dumerat.maxime@gmail.com>
2022-11-25 17:51:41 +0100

    Moved Test to folder example

--- EJBank-web/src/main/java/com/ejbank/api/Test.java
+++ EJBank-web/src/main/java/com/ejbank/api/Test.java
@@ -1,6 +1,6 @@
 package com.ejbank.api;
 
-import com.ejbank.api.payload.PeoplePayload;
+import com.ejbank.api.payloadExample.PeoplePayload;
 import com.ejbank.test.TestBeanLocal;
 import javax.ejb.EJB;
 import javax.enterprise.context.RequestScoped;
@@ -28,7 +28,7 @@ public class Test {
     
     @GET
     @Path("/people/{age}")
-    public PeoplePayload testPayloadReponse(@PathParam("age") Integer age) {
+    public PeoplePayload testPayloadResponse(@PathParam("age") Integer age) {
         return new PeoplePayload("Jean", "Dupont", age);
     }
     
