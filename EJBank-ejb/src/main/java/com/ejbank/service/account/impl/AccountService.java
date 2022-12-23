ef50404064c2b16ebc2c9e144a43d24fbe65c088
Max <dumerat.maxime@gmail.com>
2022-12-23 15:40:16 +0100

    Fixe error message

--- EJBank-ejb/src/main/java/com/ejbank/service/account/impl/AccountService.java
+++ EJBank-ejb/src/main/java/com/ejbank/service/account/impl/AccountService.java
@@ -28,7 +28,7 @@ public class AccountService implements AccountServiceLocal {
             var balanceReceiverAfter = balanceReceiver.add(amount);
             var isNotPossible = balanceSourceAfter.signum()==-1;
             var message = !isNotPossible?"Can do good":"Error : can't do";
-            return new ValidityCheckDto(!isNotPossible, balanceSourceAfter, balanceReceiverAfter, message, "");
+            return new ValidityCheckDto(!isNotPossible, balanceSourceAfter, balanceReceiverAfter, message, null);
         }
         return new ValidityCheckDto(false, amount, null, null, "Error: Id doesn't exist");
     }
