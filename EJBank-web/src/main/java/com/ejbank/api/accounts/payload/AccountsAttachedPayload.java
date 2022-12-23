605bb3f39ee288d20e9282ee6a6da02d4435da21
Max <dumerat.maxime@gmail.com>
2022-12-23 15:40:53 +0100

    new payload for attachedAccounts

--- EJBank-web/src/main/java/com/ejbank/api/accounts/payload/AccountsAttachedPayload.java
+++ EJBank-web/src/main/java/com/ejbank/api/accounts/payload/AccountsAttachedPayload.java
@@ -1,14 +1,37 @@
 package com.ejbank.api.accounts.payload;
 
+import com.ejbank.dto.AccountDto;
+
 import java.util.List;
+import java.util.Objects;
 
 public class AccountsAttachedPayload {
 
     private final List<AccountAttachedPayload> accounts;
+    private final String error;
 
-    public AccountsAttachedPayload(List<AccountAttachedPayload> accounts ) {
-        this.accounts= accounts;
+
+    private AccountsAttachedPayload(List<AccountAttachedPayload> accounts, String error) {
+        this.accounts= Objects.requireNonNull(accounts);
+        this.error = Objects.requireNonNull(error);
     }
+
+    // TODO activate the mapper when DTO is create
+
+//    public static AccountsAttachedPayload AccountsFromDTO(List<AccountAttachedPayload> accounts, String error){
+//        return new AccountsAttachedPayload(accounts.stream()
+//                               .map(accountWithUser ->
+//                        new AccountWithUserPayload(
+//                                new AccountPayload(
+//                                        accountWithUser.getId(),
+//                                        accountWithUser.getType(),
+//                                        accountWithUser.getAmount()
+//                                ),
+//                                new UserPayload(accountWithUser.getFirstName(), ""),
+//                                accountWithUser.getValidation()
+//                        ))
+//                .toList(),error);
+//    }
 
     public List<AccountAttachedPayload> getAccounts(){
         return accounts;
